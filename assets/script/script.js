4875826bd8f092c77f556a91cc9ad2ec0243c681
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2021-11-29 15:30:08 -0500

    padraodepastas

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+const hanoi  = document.getElementById("hanoi");
+const bloco1 = document.createElement("div")
+bloco1.id = "1"
+const bloco2 = document.createElement("div")
+bloco2.id = "2"
+const bloco3 = document.createElement("div")
+bloco3.id = "3"
+hanoi.appendChild(bloco1)
+hanoi.appendChild(bloco2)
+hanoi.appendChild(bloco3)
