7d4a61ac8d8dcedf30700d31b95317434094e60d
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2021-11-30 08:50:11 -0500

    refatorado e melhorado

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
 const musica = document.getElementById('musica');
+const erro = document.getElementById('erro');
 function criarEstruturaHTML() {
     const main = document.querySelector('.container');
     const section = document.createElement('section');
@@ -17,11 +18,9 @@ function criarEstruturaHTML() {
     pQtdMinJogadas.innerHTML = 'Movimentos efetuados: ';
     document.body.appendChild(pQtdMinJogadas);
     document.body.appendChild(pContadorJogadas);
-    
     main.appendChild(section);
     main.appendChild(section2);
     main.appendChild(section3);
-  
     document.body.appendChild(main);
     document.body.appendChild(resetButton);
     resetButton.addEventListener("click", resete);
@@ -30,6 +29,18 @@ criarEstruturaHTML();
 const sectionContainer = document.querySelector('.sectionContainer');
 const sectionContainer2 = document.querySelector('.sectionContainer2');
 const sectionContainer3 = document.querySelector('.sectionContainer3');
+function criarToco(){
+    let toco = document.createElement('div');
+    toco.id = `toco1`;
+    let toco2 = document.createElement('div');
+    toco2.id = `toco2`;
+    let toco3 = document.createElement('div');
+    toco3.id = `toco3`;
+    sectionContainer.appendChild(toco);
+    sectionContainer2.appendChild(toco2);
+    sectionContainer3.appendChild(toco3);
+}
+criarToco()
 function criarBlocos(numeroDeBlocos) {
     for(let i = 0; i < numeroDeBlocos; i++) {
         let blocos = document.createElement('span');
@@ -37,68 +48,51 @@ function criarBlocos(numeroDeBlocos) {
         sectionContainer.appendChild(blocos);
     }
 }
-
-
-
-function criarToco(){
-   
-        let toco = document.createElement('div');
-        toco.id = `toco1`;
-        let toco2 = document.createElement('div');
-        toco2.id = `toco2`;
-        let toco3 = document.createElement('div');
-        toco3.id = `toco3`;
-        sectionContainer.appendChild(toco);
-        sectionContainer2.appendChild(toco2);
-        sectionContainer3.appendChild(toco3);
+function selecaodejogos(){
+    const butoes = document.createElement('div');
+    butoes.classList.add('menu');
+    const main = document.querySelector('.container');
+    const opDeJogo = document.createElement('p');
+    opDeJogo.innerHTML = 'Dificuldades:';
+    butoes.appendChild(opDeJogo);
+    const easyButton = document.createElement('button');
+    easyButton.innerHTML = 'Gennin';
+    easyButton.id = 3;
+    butoes.appendChild(easyButton);
+    const mediumButton = document.createElement('button');
+    mediumButton.innerHTML = 'Chunnin';
+    mediumButton.id = 4;
+    butoes.appendChild(mediumButton);
+    const hardButton = document.createElement('button');
+    hardButton.innerHTML = 'Jonnin';
+    hardButton.id = 5;
+    butoes.appendChild(hardButton);
+    main.appendChild(butoes);
+    easyButton.addEventListener("click", trocamodal);
+    mediumButton.addEventListener("click", trocamodal);
+    hardButton.addEventListener("click", trocamodal);
+    function trocamodal(){
+        sectionContainer.classList.add('flex');
+        sectionContainer2.classList.add('flex');
+        sectionContainer3.classList.add('flex');
+        opDeJogo.classList.add('none');
+        easyButton.classList.add('none');
+        mediumButton.classList.add('none');
+        hardButton.classList.add('none');
+        criarBlocos(event.target.id);
+    }
 }
-
-musica.play()
-criarToco()
-
-
-
+selecaodejogos()
 function popupvitoria(){
     const vitoria = document.createElement('p');
    vitoria.innerHTML = 'Parabéns você venceu!';
     document.body.appendChild(vitoria);
 }
-
-function selecaodejogos(){
-    const main = document.querySelector('.container');
-    const opDeJogo = document.createElement('p');
-    opDeJogo.innerHTML = 'Dificuldades:';
-    main.appendChild(opDeJogo);
-    const easyButton = document.createElement('button');
-    easyButton.innerHTML = 'Gennin';
-    easyButton.id = 3;
-    main.appendChild(easyButton);
-    const mediumButton = document.createElement('button');
-    mediumButton.innerHTML = 'Chunnin';
-    mediumButton.id = 4;
-    main.appendChild(mediumButton);
-    const hardButton = document.createElement('button');
-    hardButton.innerHTML = 'Jonnin';
-    hardButton.id = 5;
-    main.appendChild(hardButton);
-
-
-    easyButton.addEventListener("click", trocamodal)
-    mediumButton.addEventListener("click", trocamodal)
-    hardButton.addEventListener("click", trocamodal)
-
-    function trocamodal(){
-        sectionContainer.style.display = "flex";
-        sectionContainer2.style.display = "flex";
-        sectionContainer3.style.display = "flex";
-        opDeJogo.style.display = "none";
-        easyButton.style.display = "none";
-        mediumButton.style.display = "none";
-        hardButton.style.display = "none";
-        criarBlocos(event.target.id)
-    }
-}
-selecaodejogos()
 function resete(){
     location.reload();
 }
+
+//popupvitoria()
+
+//musica.play()
+//erro.play()
